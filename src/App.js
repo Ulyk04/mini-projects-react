484fb5d4299c3cddda99ad4d64a67e40cd5d27a6
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-03-13 16:36:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 import Counter from './components/Counter';
 import Home from './components/Home';
 import ToDo from './components/ToDo';
+import Timer from './components/Timer';
+import Search from './components/Seearch';
+import Login from './components/Login';
+import Galery from './components/Galery';
+import Theme from './components/Theme';
 
 
 function App() {
@@ -13,6 +18,11 @@ function App() {
               <Route path='/' element = {<Home />} />
               <Route path='/counter' element = {<Counter/>} />
               <Route path='/list' element = {<ToDo />} />
+              <Route path='/timer' element = {<Timer />} />
+              <Route path='/search' element={<Search/>} />
+              <Route path='/login' element={<Login/>} />
+              <Route path='/images' element = {<Galery/>} />
+              <Route path='/theme' element = {<Theme />} />
           </Routes>
       </div>
     </Router>
